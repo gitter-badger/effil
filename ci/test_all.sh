1ff7c515f0a7357e653f80a1f8c32410facc349e
mihacooper <mihakuper@gmail.com>
2017-04-04 22:59:17 +0300

    improve dump reading

--- ci/test_all.sh
+++ ci/test_all.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+checkdump() { # $1 - binary name
+    COREFILE=$(find . -maxdepth 1 -name "core*" | head -n 1) # find core file
+    if [ -f "$COREFILE" ] && [ $(which gdb) ]; then
+        gdb -c "$COREFILE" ./tests -ex "thread apply all bt" -ex "set pagination 0" -batch
+    fi
+}
+
 for build_type in debug release; do
-    mkdir -p $build_type
-    (cd $build_type && cmake -DCMAKE_BUILD_TYPE=$build_type $@ .. && make -j4 install)
-    (cd $build_type && ./tests && lua run_tests.lua)
+    mkdir -p $build_type && cd $build_type
+    cmake -DCMAKE_BUILD_TYPE=$build_type $@ .. && make -j4 install
+    ./tests || checkdump ./tests
+    lua run_tests.lua || checkdump lua
 done
