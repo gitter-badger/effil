9e6dcc6abab09d385b24c13c1d4859a85c14921a
Ilia Udalov <iudalov@yandex-team.ru>
2017-06-21 00:35:12 +0300

    Add DYLD_LIBRARY_PATH

--- ci/test_all.sh
+++ ci/test_all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+export DYLD_LIBRARY_PATH="$LUA_DIR/lib:$DYLD_LIBRARY_PATH"
+
 for build_type in debug release; do
     mkdir -p $build_type
     (cd $build_type && cmake -DCMAKE_BUILD_TYPE=$build_type $@ .. && make -j4 install)
